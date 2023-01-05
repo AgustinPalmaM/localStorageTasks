28e7805cbfd0d2a1e629b588eb9015f784fa15d3
AgustinPalmaM <palma.agustin7@gmail.com>
2023-01-05 16:40:17 -0300

    add localStorage feature, now the user can see all of the tweets if the page is recharged, also can delete tweet by tweet

--- js/app.js
+++ js/app.js
@@ -7,7 +7,15 @@ let tweets = [];
 eventListeners();
 
 function eventListeners() {
+  // when the user add a new tweet
   form.addEventListener( 'submit', addTweet );
+  // when the document is ready
+  document.addEventListener('DOMContentLoaded', () => {
+    tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+    console.log(tweets)
+
+    addHtml()
+  })
 }
 
 
@@ -69,16 +77,45 @@ function addHtml() {
   if(tweets.length > 0) {
 
     tweets.forEach((tweet) => {
-      
+      // add a belete button to remove each tweet
+      const deleteButton = document.createElement('A');
+      deleteButton.classList.add('borrar-tweet');
+      deleteButton.innerText = 'X';
+
+      deleteButton.onclick = () => {
+        deleteTweet(tweet.id);
+      };
+
       const li = document.createElement('LI');
       li.innerText = tweet.tweet;
+
+      // add deleteButton to the li element
+      li.appendChild(deleteButton);
 
       tweetList.appendChild(li);
     })
 
   }
 
+  synchroLocalStorage();
+
 }
+
+// Add actual tweets to the local storage
+
+function synchroLocalStorage() {
+  localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+// delete an specific tweet
+
+function deleteTweet(id) {
+  tweets = tweets.filter( tweet => tweet.id !== id );
+  console.log(tweets)
+  addHtml()
+}
+
+// Clean the html to prevent duplicate previous tweets with actual tweets
 
 function cleanHtml() {
   
